69218670dd653b4755f6a57d016f6944e94b92e3
Kam41514 <slmbnekamil@gmail.com>
2026-07-29 14:01:49 +0300

    script.lua

--- script.lua
+++ script.lua
@@ -1221,7 +1221,7 @@ ExploitsRightGroupBox:AddToggle("AutoChestToggle", {
 
 						task.wait(0.3)
 						local hrp2 = character:WaitForChild("HumanoidRootPart")
-						hrp.CFrame = CFrame.new(0, -15, 0)
+						hrp2.CFrame = top.CFrame + Vector3.new(0, -15.33, 0)
 
                         task.wait(3.2)
 
