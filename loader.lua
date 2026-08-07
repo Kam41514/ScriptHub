695a4ad1de5ccbd9a0c6c05387846212af2ad8e8
Kam41514 <slmbnekamil@gmail.com>
2026-08-07 11:41:12 +0300

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,9 @@
 if game.PlaceId == 76606442853797 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Kam41514/ScriptHub/main/script.lua"))()
     else
+if game.PlaceId == 10266164381 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kam41514/ScriptHub/refs/heads/main/scriptbeta.lua"))()
+    else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Kam41514/ScriptHub/main/universal.lua"))()
+    end
 end
