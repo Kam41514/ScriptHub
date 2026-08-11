a8604d14ffd42d06f37fa2eccaced7c95df4bbe2
Kam41514 <slmbnekamil@gmail.com>
2026-08-11 06:55:40 +0300

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,7 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
 if game.PlaceId == 76606442853797 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Kam41514/ScriptHub/main/script.lua"))()
     else
